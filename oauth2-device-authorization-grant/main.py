57916a6f960ed12a6c8443ffc9becc0bdab3b372
Matthias Binzer <matthias.binzer@de.bosch.com>
2021-10-08 16:33:32 +0200

    auth: I40 Testbed integration changes
    Summary of development changes:
    * Keycloak config: Update with external (vc-authn) identity provider
    * Add vc-authn from bcgov/vc-authn-oidc module
      Image built from https://github.com/bcgov/vc-authn-oidc sources
      (multi-stage build Dockerfile)
    * Add vc-authn to overall project docker-compose
      Auto-import from config directory
    * Add option DISABLE_SSL_VERIFY env variable
    * auth: Make ports configurable
    * device-grant: Remove debugging output. Do not open browser automatically
    * Restrictions on schema_id
      to let the app choose from existing credentials
    * keycloak export with auto mapping firstName, lastName
    * Keycloak behind proxy
    * Disable SSL for keycloak to use docker internal http token ep
    * use appsettings env instead of pgsql import script
    * KEYCLOAK_PASSWORD in .evn
    * import script to wait until API accepted config upload
    * Example with did restrictions
    * flexible auth url consts also possible to get from env
    * interval default 5
    * env example
    
    Signed-off-by: Matthias Binzer <matthias.binzer@de.bosch.com>

--- oauth2-device-authorization-grant/main.py
+++ oauth2-device-authorization-grant/main.py
@@ -16,50 +16,57 @@ import os
 BASE_URL = os.environ.get('BASE_URL') or 'http://localhost:8080'
 CLIENT_ID = os.environ.get('CLIENT_ID') or 'devicegrant'
 REALM = os.environ.get('REALM') or 'test'
+DISABLE_SSL_VERIFY = os.environ.get('DISABLE_SSL_VERIFY', 'False').lower() in ('true', '1')
 
-DEVICE_GRANT_ENDPOINT = BASE_URL + '/auth/realms/' + REALM + '/protocol/openid-connect/auth/device'
-TOKEN_ENDPOINT = BASE_URL + '/auth/realms/' + REALM + '/protocol/openid-connect/token'
+DEVICE_GRANT_ENDPOINT = os.environ.get('DEVICE_GRANT_ENDPOINT') or BASE_URL + '/auth/realms/' + REALM + '/protocol/openid-connect/auth/device'
+TOKEN_ENDPOINT = os.environ.get('TOKEN_ENDPOINT') or BASE_URL + '/auth/realms/' + REALM + '/protocol/openid-connect/token'
 USERINFO_ENDPOINT = BASE_URL + '/auth/realms/' + REALM + '/protocol/openid-connect/userinfo'
 CERTIFICATE_ENDPOINT = BASE_URL + '/auth/realms/' + REALM + '/protocol/openid-connect/certs'
 
 def pprint(j):
     print(json.dumps(j, indent=4))
 
-r = requests.post(DEVICE_GRANT_ENDPOINT, {'client_id': CLIENT_ID})
+session = requests.Session()
+if DISABLE_SSL_VERIFY:
+    session.verify = False
+
+r = session.post(DEVICE_GRANT_ENDPOINT, {'client_id': CLIENT_ID})
 rj = r.json()
 device_grant_result = rj
 device_code = rj['device_code']
+interval = rj.get('interval', 5)
 pprint(rj)
 
 try:
     time.sleep(3)
-    webbrowser.open_new_tab(device_grant_result['verification_uri_complete'])
+    #webbrowser.open_new_tab(device_grant_result['verification_uri_complete'])
 except:
     print('Could not open browser, please try it manually')
 
+print('Use this URL to signin: ', device_grant_result['verification_uri_complete'])
+print('Polling authentication state...')
 token_result = None
 while True:
-    print('Polling authentication state...')
     data = {
         'grant_type': 'urn:ietf:params:oauth:grant-type:device_code',
         'client_id': CLIENT_ID,
         'device_code': device_code
     }
-    r = requests.post(TOKEN_ENDPOINT, data)
+    r = session.post(TOKEN_ENDPOINT, data)
     rj = r.json()
     if 'error' in rj:
         #pprint(rj)
-        print('Server Reply: ', rj['error'])
-        print('Server Reply: ', rj['error_description'])
-        print('Use this URL to signin: ', device_grant_result['verification_uri_complete'])
-        time.sleep(3)
+        #print('Server Reply: ', rj['error'])
+        #print('Server Reply: ', rj['error_description'])
+        #print('Use this URL to signin: ', device_grant_result['verification_uri_complete'])
+        time.sleep(interval)
     else:
         token_result = rj
         break
 
 pprint(token_result)
 
-r = requests.post(USERINFO_ENDPOINT, {'access_token': token_result['access_token']})
+r = session.post(USERINFO_ENDPOINT, {'access_token': token_result['access_token']})
 userinfo = r.json()
 print()
 at = jwt.decode(token_result['access_token'], options={"verify_signature": False})
