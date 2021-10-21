617ca96139951a2aa4b9630ae2bc90afbe7369c0
Matthias Binzer <matthias.binzer@de.bosch.com>
2021-10-21 13:41:02 +0200

    issuer: Initial version of a VC issuer UI
    Summary of development changes:
    * Make ports configurable
    * Dockerfile
    * docker-compose
    * webhook
    * Add wait for acapy
    * Fix build configuration
    * Fix reverse proxy issue with subfolder redirect
    * Add logout
    * Generate error on docker-compose up to generate a new seed
    * Polish the issued VC and connection request a bit
    * cred_def_id debug output
    * do not expose acapy admin port
    
    Signed-off-by: Matthias Binzer <matthias.binzer@de.bosch.com>

---
+++ acapy-issuer-ui/layouts/default.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app>
+
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <v-btn to="/" text>Credential Issuer UI</v-btn>
+      <v-spacer></v-spacer>
+
+      <v-app-bar-nav-icon />
+    </v-app-bar>
+
+    <v-main class="ma-5">
+      <nuxt />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+}
+</script>
